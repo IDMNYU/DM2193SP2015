b016a459ac323f56968c0b2f778f66c3196c2310
unknown <alvi.kabir@nyu.edu>
2015-04-01 02:22:09 -0400

    added js

--- awesome-web-class/kabir_alvi/learning_log/intro_to_js/scripts/scripts.js
+++ awesome-web-class/kabir_alvi/learning_log/intro_to_js/scripts/scripts.js
@@ -52,33 +52,49 @@
 // 
 
 // Events
-console.log('before');
-
+//console.log('before');
+//var myHeading  = document.getElementById('my-heading');
+//
+//var myHeading2  = document.getElementsByClassName('my-heading')[0];
+//console.log(myHeading === myHeading2);
+var myBox = document.createElement('div');
+myBox.classList.add('my-box');
+myBox.innerHTML='My Box';
+document.body.appendChild(myBox);
+myBox.remove();
+console.log(myBox);
+var myButton = document.getElementsByClassName('my-button')[0];
+var parentDiv = myButton.parentNode;
+parentDiv.insertBefore(myBox,myButton);
+myButton.addEventListener('click',function(e){
+    console.log(e.currentTarget);
+    myBox.classList.toggle('inactive');
+});
 // window.onload = function(){
 //     console.log('after');
 // }
-
-document.addEventListener('DOMContentLoaded', function(event) {
-    console.log('in between');
-});
-
-document.addEventListener('DOMContentLoaded', function(event) {
-    console.log('again');
-});
-
-document.addEventListener('DOMContentLoaded', function(event) {
-    console.log('and again');
-});
+//
+//document.addEventListener('DOMContentLoaded', function(event) {
+//    console.log('in between');
+//});
+//
+//document.addEventListener('DOMContentLoaded', function(event) {
+//    console.log('again');
+//});
+//
+//document.addEventListener('DOMContentLoaded', function(event) {
+//    console.log('and again');
+//});
 
 // window.onload = function(){
 //     console.log('oops');
 // }
 
-window.addEventListener('load', function(){
-    console.log('after');
-});
-
-window.addEventListener('load', function(){
-    console.log('after 2');
-});
-
+//window.addEventListener('load', function(){
+//    console.log('after');
+//});
+//
+//window.addEventListener('load', function(){
+//    console.log('after 2');
+//});
+//
