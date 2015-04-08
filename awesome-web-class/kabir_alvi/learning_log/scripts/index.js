6737d82aa7ece532947eab3143308c5735579bbe
Alvi <alvi.kabir@nyu.edu>
2015-04-07 22:38:59 -0400

    added scrolling js

--- awesome-web-class/kabir_alvi/learning_log/scripts/index.js
+++ awesome-web-class/kabir_alvi/learning_log/scripts/index.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
         var href = $(this).attr('href');
         console.log(typeof (href));
         console.log($(href));
+        $('a[href^="#"]').on('click',function (e) {
+            e.preventDefault();
 
+            var target = this.hash;
+            var $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing');
+        });
     });
 });
