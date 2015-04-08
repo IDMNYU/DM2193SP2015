085db568dbbda807bf6c2aede018aeaffa0c3d97
Alvi <alvi.kabir@nyu.edu>
2015-04-08 14:21:31 -0400

    added toggling

--- awesome-web-class/kabir_alvi/learning_log/scripts/index.js
+++ awesome-web-class/kabir_alvi/learning_log/scripts/index.js
@@ -1,20 +1,31 @@
 /**
  * Created by Alvi on 4/7/2015.
  */
-$(document).ready(function(){
-    $(".entry-link").click(function(){
-        var href = $(this).attr('href');
-        console.log(typeof (href));
-        console.log($(href));
-        $('a[href^="#"]').on('click',function (e) {
-            e.preventDefault();
-
-            var target = this.hash;
-            var $target = $(target);
-
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top
-            }, 900, 'swing');
-        });
+function scrollToElement(element){
+    var href = $(element).attr('href');
+    $("a[href^='#']").on('click',function (e) {
+        e.preventDefault();
+        var target = element.hash;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900);
     });
+}
+
+$(document).ready(function(){
+    $("h1:first").hide().fadeIn(1000);
+    $(".entry-link").click(function(){
+        scrollToElement(this);
+    });
+    $(".hide-entry-text").click(function(){
+        var $d = $(this).parent();
+        console.log($(this));
+        $d.next().slideToggle(1000);
+    });
+    //toggle(function(){
+    //    this.parent().hide();
+    //},function(){
+    //    this.parent().show();
+    //});
 });
