103ef011d073f032535aa9bc1e1ff1d38498bdb7
LeviMollison <lm2817@nyu.edu>
2015-04-21 22:00:57 -0400

    Directory added

---
+++ awesome-web-class/mollison_levi/final-project/nuzlocke-content/diabeticgirl4/chp-1.js
@@ -0,0 +1,27 @@
+var story = '
+<p>Hi! I\'m Charmander. I\'m kept at the large laboratory building in Pallet Town, with two other Pokémon (more about them later), Professor Oak, and his busy aides. Most of the time I am left inside my pokéball on hours at end while Oak is away, but I don\'t mind. It\'s somewhat relaxing, doing nothing but listening to the constant chattering among the assistants about the latest pokédex glitches or newest TM findings.
+</p>
+<p>I don\'t mind the simple life, but at times I find myself wishing for something more- freedom, adventure, or at least a little excitement now and then. I long to see the open sky and the rustling grass around me. So when Oak released the three of us from our pokéballs and notified us that some new trainers would come by to pick out a starter Pokémon for themselves, a great excitement burned within me, brighter than the fire on the tip of my tail.
+</p>
+<p>
+What would the future hold? Would I, the energetic and brave Charmander, be chosen over the loud and boisterous Squirtle or the quiet and kind Bulbasaur? What would the trainers be like? Young or old, sincere or unpleasant? Would I be chosen to share an exciting adventure with this person, or would I be put aside to wait for the next trainer to want me?
+</p>
+<p>"What are you hoping you\'ll get?" I asked the Bulbasaur beside me. He looked thoughtful, but then smiled.
+</p>
+<p>"I don\'t want much, I suppose. I just want a nice trainer to take me wherever they go, and as long as they are happy, I\'ll be happy," he said in a soft and satisfied tone.
+</p>
+<p>"Bah, that sounds boring!" exclaimed the Squirtle. I suppressed the urge to growl at him; if I had wanted his opinion, I would have asked for it. "I want to train to become strong and be better than all the Pokémon in all of Kanto," he continued. I sighed, and ignored the rest of what the Squirtle kept babbling about. He wasn\'t mean, thankfully, but he had a competitive nature that made it rather annoying whenever he felt the need to one-up somebody.
+</p>
+<p>"What about you, Charmander? What are your dreams?" asked the curious Bulbasaur.
+</p>
+<p>"I… I want to venture far across the land, to see everything there is to see. I want to meet all sorts of Pokémon, and to test my strength in battles. I guess I just want to have a fun, exciting life," I admitted.
+</p>
+<p>Before the Bulbasaur could respond, Professor Oak came strolling into view.
+</p>
+<p>"Boy, aren\'t you fellows talkative," he chuckled. "I sure do wish I could understand what you were talking about. Perhaps that could be the next big invention: a Human to Pokémon translator…" He then turned to his large filing cabinet and began whistling a tune to himself.
+</p>
+<p>"I do find it silly," whispered the Bulbasaur, "that we Pokémon can understand human speech easily, yet humans are too dense to understand even the simplest of things we have to say." I nodded, and even the Squirtle wouldn\'t disagree.
+</p>
+<p>Suddenly, Oak stopped whistling. "Oh, is it nearly 2 o\'clock already? The kids will be here any minute. Sorry, but you guys need to go back into your pokéballs," he told us. I looked at the front door then back at Oak, and then I was sucked into the emptiness of the pokéball.
+</p>
+';
