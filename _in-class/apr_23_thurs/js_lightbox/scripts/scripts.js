85e3c196f09639da8a3ef1d3491711eda1619439
Johnny Benson <johnny@tumblr.com>
2015-04-23 21:04:56 -0400

    Lightbox

--- _in-class/apr_23_thurs/js_lightbox/scripts/scripts.js
+++ _in-class/apr_23_thurs/js_lightbox/scripts/scripts.js
@@ -1,10 +1,8 @@
 var setupLightbox = function($lightbox) {
 
-    // Grab all of the DOM elements
     var $lightboxControls = $lightbox.find('[data-lightbox-control]');
     var $lightboxLabel = $lightbox.find('[data-lightbox-label]');
     var $lightboxItems = $lightbox.find('[data-lightbox-item]');
-
     var item_count = $lightboxItems.length;
     var current_index = 0;
     var next_index = 0;
@@ -17,43 +15,34 @@ var setupLightbox = function($lightbox) {
     
     // Hide Next & Prev Buttons
     } else if (item_count === 1) {
-        
         $lightboxControls.hide();
 
     } else {
 
-        // Listen for Clicks
-        $lightboxControls.on('click', function(e){
-                
-            // Control that was clicked
-            var $btn = $(this);
+        $lightboxControls.on('click', function(){
+            
+            $btn = $(this);
 
-            // Fetch current index
             current_index = $lightbox.find('.active[data-lightbox-item]').index();
             
-            // Update next index with previous
             if ($btn.data('lightbox-control') === 'previous') {
                 next_index = (current_index === 0) ? item_count-1 : current_index-1;
             }
             
-            // Update next index with next
             if ($btn.data('lightbox-control') === 'next') {
                 next_index = (current_index === item_count-1) ? 0 : current_index+1;
             }
 
-            // Clear active from all lightbox items
             $lightboxItems.removeClass('active');
 
-            // Show the next lightbox item
             $lightboxCurrentItem = $lightboxItems.eq(next_index);
             $lightboxCurrentItem.addClass('active');
 
-            // Update the lightbox label
             $lightboxLabel.text($lightboxCurrentItem.find('img').attr('alt'));
-
         });
-
     }
+
+    $lightboxLabel.text($lightboxCurrentItem.find('img').attr('alt'));
 
     $lightbox.addClass('active');
 
@@ -63,21 +52,19 @@ var setupLightbox = function($lightbox) {
 
 var destroyLightbox = function($lightbox) {
 
-    // Grab all of the DOM elements
-    // UNDO everything we did above
+    var $lightboxControls = $lightbox.find('[data-lightbox-control]');
+    $lightboxControls.off('click');
+    
+    var $lightboxLabel = $lightbox.find('[data-lightbox-label]');
+    $lightboxLabel.text($lightboxLabel.data('lightbox-label'));
+
+    $lightbox.find('.active[data-lightbox-item]').removeClass('active');
+    $lightbox.find('[data-lightbox-item]').eq(0).addClass('active');
 
     $lightbox.removeClass('active');
 
     var $glass = $('[data-lightbox-glass]');
     $glass.removeClass('active');
-
-    var $lightboxControls = $lightbox.find('[data-lightbox-control]');
-
-    // Stop listening for clicks
-    $lightboxControls.off('click');
-
-    // Reset the active class to the first item
-    // Reset the label to the default value or the first item label
 };
 
 $('[data-open-lightbox]').on('click', function(){
