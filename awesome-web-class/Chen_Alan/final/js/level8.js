75da3dd6d833c31e7ca11e5992d37a1e8ac65f71
Alan Chen <alanchen@nyu.edu>
2015-04-28 18:58:43 -0400

    final update

--- awesome-web-class/Chen_Alan/final/js/level8.js
+++ awesome-web-class/Chen_Alan/final/js/level8.js
@@ -1,9 +1,11 @@
+document.cookie="cookie=cookies_are_yummy";
+
 function validateForm()
 {
 	var hash = CryptoJS.MD5($("#password").val());
 	hash = hash.toString(CryptoJS.enc.Hex);
 	console.log(hash);
-	if (hash == "4fa93b7a802d62792311285119bbb88f") { window.location.href = "level6.html" } else {
+	if (hash == "a47a4407524c25bbe5f8d4c1450b3f8d") { window.location.href = "level9.html" } else {
 		alert("Wrong password");
 	}
   return false;
--- awesome-web-class/Chen_Alan/final/js/level9.js
+++ awesome-web-class/Chen_Alan/final/js/level9.js
@@ -3,7 +3,7 @@ function validateForm()
 	var hash = CryptoJS.MD5($("#password").val());
 	hash = hash.toString(CryptoJS.enc.Hex);
 	console.log(hash);
-	if (hash == "4fa93b7a802d62792311285119bbb88f") { window.location.href = "level6.html" } else {
+	if (hash == "88d9fbd1ce0577c05f76891fd121c47c") { window.location.href = "level10.html" } else {
 		alert("Wrong password");
 	}
   return false;
