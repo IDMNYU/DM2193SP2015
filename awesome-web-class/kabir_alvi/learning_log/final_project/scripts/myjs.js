10910073560de3ebab3b3f3d45a2d1636af37d94
unknown <alvi.kabir@nyu.edu>
2015-04-28 23:56:41 -0400

    angular stuff

--- awesome-web-class/kabir_alvi/learning_log/final_project/scripts/myjs.js
+++ awesome-web-class/kabir_alvi/learning_log/final_project/scripts/myjs.js
@@ -3,13 +3,8 @@
  */
 $(document).ready(function(){
     $(".verb.past").text('[Verb/Past]');
-    $(".adjective").text('[Adjective]');
-    $(".noun").text('[Noun]');
-    //$('.dropdown-menu>li>a').on('click',function(){
-    //    $('.nav-tabs>li').removeClass('active');
-    //    var $href = $(this).attr('href');
-    //    var $selectedTab = $('.nav-tabs>li>a[href="'+$href+'"]');
-    //    console.log($selectedTab.parent());
-    //    $selectedTab.parent().addClass('active');
-    //});
+    $("span[data-type='pronoun']").text('[Pronoun]');
+    console.log($("span[data-type='pronoun']"));
+    //$(".adjective").text('[Adjective]');
+    //$(".noun").text('[Noun]');
 });
