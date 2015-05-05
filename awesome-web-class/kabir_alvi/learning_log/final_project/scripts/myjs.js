a63e021799caf03dbef46fb25f1c4937ee604032
Alvi <alvi.kabir@nyu.edu>
2015-05-05 04:08:45 -0400

    angular changes and using materialize now

--- awesome-web-class/kabir_alvi/learning_log/final_project/scripts/myjs.js
+++ awesome-web-class/kabir_alvi/learning_log/final_project/scripts/myjs.js
@@ -1,10 +1,10 @@
 /**
  * Created by Alvi on 4/16/2015.
  */
-$(document).ready(function(){
-    $(".verb.past").text('[Verb/Past]');
-    $("span[data-type='pronoun']").text('[Pronoun]');
-    console.log($("span[data-type='pronoun']"));
-    //$(".adjective").text('[Adjective]');
-    //$(".noun").text('[Noun]');
-});
+//$(document).ready(function(){
+//    $(".verb.past").text('[Verb/Past]');
+//    $("span[data-type='pronoun']").text('[Pronoun]');
+//    console.log($("span[data-type='pronoun']"));
+//    //$(".adjective").text('[Adjective]');
+//    //$(".noun").text('[Noun]');
+//});
