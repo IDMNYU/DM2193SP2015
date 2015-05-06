5a452019b878abe9d9e817522b53e59a78bd5b47
Alvi Kabir <alvi.kabir@nyu.edu>
2015-05-05 21:32:41 -0400

    added story 3

--- awesome-web-class/kabir_alvi/learning_log/final_project/scripts/myjs.js
+++ awesome-web-class/kabir_alvi/learning_log/final_project/scripts/myjs.js
@@ -1,10 +1,3 @@
 /**
  * Created by Alvi on 4/16/2015.
  */
-//$(document).ready(function(){
-//    $(".verb.past").text('[Verb/Past]');
-//    $("span[data-type='pronoun']").text('[Pronoun]');
-//    console.log($("span[data-type='pronoun']"));
-//    //$(".adjective").text('[Adjective]');
-//    //$(".noun").text('[Noun]');
-//});
