39c5bd63e18b3e5e2d4babcabf12f76dabce648b
hillarychin <hc1297@nyu.edu>
2015-05-14 18:48:41 -0400

    Fixing up some stuff

--- awesome-web-class/chin_hillary/learning_log/final/js/scripts.js
+++ awesome-web-class/chin_hillary/learning_log/final/js/scripts.js
@@ -1,11 +1,10 @@
-console.log("is it working at all");
+//console.log("is it working at all");
 
 $('[store-player-choice]').on('click', function(){
-    console.log("please work");
+    //console.log("please work");
     var clicked = $(event.target).attr('class');    
     sessionStorage.setItem('player-choice', clicked);
-    console.log(clicked);
+    //console.log(clicked);
     var playerChosen = sessionStorage.getItem('player-choice');
-    console.log(playerChosen);
-    console.log("Toofast?");
+    //console.log(playerChosen);
 });
