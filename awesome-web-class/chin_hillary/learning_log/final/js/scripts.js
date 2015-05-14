1ff45c450b589169ccda685e71eeecb6d9ebf5b9
hillarychin <hc1297@nyu.edu>
2015-05-14 16:43:46 -0400

    Final Project and Learning Log

---
+++ awesome-web-class/chin_hillary/learning_log/final/js/scripts.js
@@ -0,0 +1,11 @@
+console.log("is it working at all");
+
+$('[store-player-choice]').on('click', function(){
+    console.log("please work");
+    var clicked = $(event.target).attr('class');    
+    sessionStorage.setItem('player-choice', clicked);
+    console.log(clicked);
+    var playerChosen = sessionStorage.getItem('player-choice');
+    console.log(playerChosen);
+    console.log("Toofast?");
+});
