cbc9195dc8a102b4403c8adcb87e77d41ae50185
gitHubCal <co684@nyu.edu>
2015-05-18 00:05:42 -0400

    Folder cleanup
    .

--- awesome-web-class/Ong_Calvin/Final/puzzle.js
+++
@@ -1,4 +0,0 @@
-function checkPuzzleTime() {
-    //If time > 0:05:00, then wrong.gif
-    //else correct.gif
-}
--- awesome-web-class/Ong_Calvin/Final/scripts/riddle.js
+++ awesome-web-class/Ong_Calvin/Final/scripts/riddle.js
@@ -4,40 +4,73 @@ var riddleNum = Number($.cookie('my_number'));
 
 /*Modal Box checks answer*/
 function displayModalBox() {
-    var riddleAnswer = ['75','25','iron','devil','July 16'];
+    var riddleAnswer = ["75","25","iron","devil","July 16"];
     var riddleGuess = 0;
+    //var breakOutOfLoop = false;
     while(riddleGuess <= 5) {
-        /*$(document).ready(function (e){
-            $('#'+riddleNum+'submit').on('click',function (){
-                var answer = $('#'+riddleNum+'input').val();
+        var answer = prompt('Answer here ',' ');
+        riddleGuess++;
+        if(answer != riddleAnswer[riddleNum]) {
+            $(function() {
+                var object = $('<img>')
+                .attr('id','tempImg')
+                .attr('src', '../images/gif/wrong.gif')
+                .attr('width', '400px')
+                .attr('height', '400px')
+                .appendTo('.contentContainer')
+                .css('position', 'absolute')
+                .css('top', 'calc(50% - 400px/2)')
+                .css('left', 'calc(50% - 400px/2)');
             });
-        });*/
-        
-        var answer = prompt('Answer question here: ',' ');
-        if(answer == riddleAnswer[riddleNum]) {
-            alert('nice');
-            break;
-            //nice.gif
+            setTimeout(function(){document.getElementById('tempImg').style.display = 'none';},1000);   
         }
         else {
-            alert('wrong');
-            riddleGuess++;
-            //wrong.gif
-        }      
-  
+            break;
+        }
+        /*var answer = $('#input' + riddleNum).val();
+        $(document).ready(function(e) {
+            $('#submit' + riddleNum).on('click',function() { 
+                riddleGuess++;
+                if(answer != riddleAnswer[riddleNum]) {
+                    $(function() {
+                        var object = $('<img>')
+                        .attr('id','tempImg')
+                        .attr('src', '../images/gif/wrong.gif')
+                        .attr('width', '400px')
+                        .attr('height', '400px')
+                        .appendTo('.contentContainer')
+                        .css('position', 'absolute')
+                        .css('top', 'calc(50% - 400px/2)')
+                        .css('left', 'calc(50% - 400px/2)');
+                    });
+                    setTimeout(function(){document.getElementById('tempImg').style.display = 'none';},1000);
+                }
+                else {
+                    breakOutOfLoop = true;
+                }
+            });
+        });*/
     }
-    
     if(riddleGuess > 5) {
-        alert('fail');     
-        //fail.gif
+        setTimeout(function() {
+        $(function() {
+            var object = $('<img>')
+            .attr('src', '../images/gif/denzel.gif')
+            .attr('width', '400px')
+            .attr('height', '400px')
+            .appendTo('.contentContainer')
+            .css('position', 'absolute')
+            .css('top', 'calc(50% - 400px/2)')
+            .css('left', 'calc(50% - 400px/2)');
+        })}, 100);
     }
-    
 }
 
 function displayRiddle() {              
    while(riddleNum <= 4) {
         document.getElementById('riddle' + riddleNum).style.display = 'block';
         displayModalBox();
+        document.getElementById('riddle' + riddleNum).style.display = 'none';
         var nextRiddle = riddleNum++;
         $.cookie('my_number',nextRiddle.toString());     
     }
